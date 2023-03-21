addc7c8d726563cb02ba8eb8377cc9b09d9d2f3d
Daniel Carvalho <32563295+CaxambuDaniel@users.noreply.github.com>
2023-03-21 09:41:46 +0000

    Update class main - Criando cabeçalho

---
+++ limpeza_compactacao_arquivos_de_carga/src/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#_*_ coding: utf-8 _*_
+#----------------------------------------------------------------------------
+#Created By: Daniel Carvalho de Souza (Caxambu)
+#Created Date: 16/01/2023
+#Last Update: 21/03/2023
+# Version: '1.0'
+#----------------------------------------------------------------------------
+""" Criado para realizar a limpeza e compactação dos arquivos de carga da solução autorize (/dados/autorize/) """
+#----------------------------------------------------------------------------
