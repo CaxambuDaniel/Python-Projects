c42b2999800c9cf90931bca1f7acdee3ca9d862c
Daniel Carvalho <32563295+CaxambuDaniel@users.noreply.github.com>
2023-03-22 14:47:22 +0000

    Update logger - alterando para horario de brasilia

--- limpeza_compactacao_arquivos_de_carga/src/modules/logger_config.py
+++ limpeza_compactacao_arquivos_de_carga/src/modules/logger_config.py
@@ -1,10 +1,26 @@
-# logger_config.py
 import logging
+from logging.handlers import TimedRotatingFileHandler
+import arrow
+
+class BrasiliaTimeFormatter(logging.Formatter):
+    def formatTime(self, record, datefmt=None):
+        dt = arrow.get(record.created).to('America/Sao_Paulo')
+        
+        if datefmt:
+            formatted_time = dt.format(datefmt)
+        else:
+            formatted_time = dt.strftime(self.default_time_format)
+        return formatted_time
 
 def configure_logger():
-    logging.basicConfig(filename="/workspaces/Projetos_Python/limpeza_compactacao_arquivos_de_carga/src/log/limpeza_compactacao_carga.log",
-                        format="%(levelname)s %(asctime)s - %(message)s",
-                        filemode='w')
+    log_format = "%(levelname)s %(asctime)s - %(message)s"
+    formatter = BrasiliaTimeFormatter(log_format)
+    
+    log_file = "/workspaces/Projetos_Python/limpeza_compactacao_arquivos_de_carga/src/log/limpeza_compactacao_carga.log"
+    handler = TimedRotatingFileHandler(log_file, when="midnight")
+    handler.setFormatter(formatter)
+
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
     return logger
