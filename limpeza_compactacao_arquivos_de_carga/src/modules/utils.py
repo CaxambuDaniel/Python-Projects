52fc2b910ef1a32efe9c865170fed2e44bf62e3a
Daniel Carvalho <32563295+CaxambuDaniel@users.noreply.github.com>
2023-03-22 16:40:08 +0000

    Update project - comentando o código

--- limpeza_compactacao_arquivos_de_carga/src/modules/utils.py
+++ limpeza_compactacao_arquivos_de_carga/src/modules/utils.py
@@ -1,8 +1,14 @@
-# utils.py
 import os
 
 def gera_lista(comando, arquivo):
+    # Executa o comando fornecido (geralmente um comando do sistema operacional) e salva a saída no arquivo especificado.
     os.system(comando)
+
+    # Abre o arquivo especificado em modo de leitura.
     with open(arquivo) as f:
+        # Lê todas as linhas do arquivo e remove os caracteres de nova linha e espaços em branco no início e no final de cada linha.
+        # Adiciona cada linha limpa à lista.
         lista = [line.strip('\n').strip() for line in f.readlines()]
+
+    # Retorna a lista de linhas limpas.
     return lista
