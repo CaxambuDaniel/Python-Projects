442f1c0e2c8b3b53e9fb572df6c73353382a7866
Daniel Carvalho <32563295+CaxambuDaniel@users.noreply.github.com>
2023-03-22 13:41:03 +0000

    create module utils

--- limpeza_compactacao_arquivos_de_carga/src/modules/gera_listas.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def gera_lista(comando, arquivo):
-    os.system(comando)
-    abre_arquivo = open(arquivo)
-    geralista = abre_arquivo.readlines()
-    lista =[line.strip('\n').strip() for line in geralista]
-    return lista
