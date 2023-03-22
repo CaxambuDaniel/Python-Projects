c0b622918676007eaed7d2983d997ce23740fda2
Daniel Carvalho <32563295+CaxambuDaniel@users.noreply.github.com>
2023-03-22 16:40:08 +0000

    Update project - comentando o código

--- limpeza_compactacao_arquivos_de_carga/src/main.py
+++ limpeza_compactacao_arquivos_de_carga/src/main.py
@@ -3,17 +3,25 @@ from modules.utils import gera_lista
 from modules.logger_config import configure_logger
 from modules.client_operations import (gera_lista_clientes, gera_lista_qtd_dir, compacta_arquivos_carga)
 
+# Função principal que executa o processo de limpeza e compactação de arquivos de carga
 def main():
+    # Configura o logger
     logger = configure_logger()
     logger.debug("Iniciando o processo de limpeza e compactação de arquivos de carga")
-    
+
+    # Gera a lista de clientes
     lista_clientes = gera_lista_clientes(logger)
     logger.debug(f"Lista de clientes gerada: {lista_clientes}")
-    
+
+    # Compacta os arquivos de carga dos clientes
     compacta_arquivos_carga(lista_clientes, logger)
-    
+
+    # Gera a lista de quantidade de diretórios para cada cliente
     gera_lista_qtd_dir(lista_clientes, logger)
+
+    # Registra o fim do processo de limpeza e compactação de arquivos de carga
     logger.debug("Processo de limpeza e compactação de arquivos de carga concluído")
 
+# Executa a função principal quando o script é executado diretamente
 if __name__ == "__main__":
     main()
--- limpeza_compactacao_arquivos_de_carga/src/modules/logger_config.py
+++ limpeza_compactacao_arquivos_de_carga/src/modules/logger_config.py
@@ -2,10 +2,14 @@ import logging
 from logging.handlers import TimedRotatingFileHandler
 import arrow
 
+# Cria uma subclasse personalizada de logging.Formatter para lidar com o fuso horário de Brasília.
 class BrasiliaTimeFormatter(logging.Formatter):
+    # Sobrescreve o método formatTime para converter o horário do registro para o fuso horário de Brasília.
     def formatTime(self, record, datefmt=None):
+        # Converte o tempo do registro para um objeto arrow e ajusta para o fuso horário de Brasília.
         dt = arrow.get(record.created).to('America/Sao_Paulo')
         
+        # Formata o tempo do registro de acordo com o formato especificado ou o formato padrão.
         if datefmt:
             formatted_time = dt.format(datefmt)
         else:
@@ -13,14 +17,27 @@ class BrasiliaTimeFormatter(logging.Formatter):
         return formatted_time
 
 def configure_logger():
+    # Define o formato dos registros de log.
     log_format = "%(levelname)s %(asctime)s - %(message)s"
+    
+    # Cria um objeto formatter usando a classe BrasiliaTimeFormatter.
     formatter = BrasiliaTimeFormatter(log_format)
     
+    # Define o caminho e o nome do arquivo de log.
     log_file = "/workspaces/Projetos_Python/limpeza_compactacao_arquivos_de_carga/src/log/limpeza_compactacao_carga.log"
+    
+    # Cria um manipulador de log que rotaciona o arquivo de log à meia-noite.
     handler = TimedRotatingFileHandler(log_file, when="midnight")
+    
+    # Define o formatter para o manipulador de log.
     handler.setFormatter(formatter)
 
+    # Configura o logger e define o nível de log para DEBUG.
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
+    
+    # Adiciona o manipulador de log ao logger.
     logger.addHandler(handler)
+    
+    # Retorna o logger configurado.
     return logger
